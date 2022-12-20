3a2f571058ca800bdd93cfdbb8d7c77ca9ca314b
vinhpt <ifromchauphong@gmail.com>
2022-12-20 16:18:13 +0000

    Queries number round 75

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,5 +2,8 @@
 python main.py \
 --dataset_file thyroid \
 --data_path data/thyroid \
---output_dir output/thyroid_dcm_icr_f10 \
---resume weights/detr-r50-e632da11.pth
+--batch_size 8 \
+--dilation \
+--set_cost_class 3 \
+--output_dir output/thyroid_dc5_dcm_icr_f10 \
+--resume weights/detr-r50-dc5-panoptic-da08f1b1.pth
